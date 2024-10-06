7530d8c424c7320ac97e03409572b3c2df364c44
timur <t20033002@gmail.com>
2024-10-07 02:45:24 +0300

    примерные сервисы, которые поднадобятся

---
+++ api-gateway/src/test/java/com/fast/food/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.food.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka-server/src/test/java/com/fast/food/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.food.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
